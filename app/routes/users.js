7ee8f90459f2642f4649b07debd079ed29769614
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-09-04 13:30:24 -0300

    Atualizando

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user.js');
 const jwt = require('jsonwebtoken');
+const withAuth = require('../middlewares/auth');
 require('dotenv').config()
 const secret = process.env.JWT_TOKEN;
 
@@ -28,13 +29,48 @@ router.post('/login', async function (req, res) {
         if (!same) {
           res.status(401).json({ message: "Incorrect email or password" });
         } else {
-          const token = jwt.sign({email}, secret, { expiresIn: '1d' });
+          const token = jwt.sign({ email }, secret, { expiresIn: '1d' });
           res.json({ user: user, token: token });
         }
       })
     }
   } catch (error) {
     res.status(500).json({ error: "Internal error, please try again." });
+  }
+});
+router.put('/', withAuth, async function (req, res) {
+  const { name, email } = req.body;
+
+  try {
+    var user = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $set: { name: name, email: email } },
+      { upsert: true, 'new': true },
+    );
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+});
+router.put('/password', withAuth, async function (req, res) {
+  const { password } = req.body;
+
+  try {
+    var user = await User.findOne({ _id: req.user._id })
+    user.password = password;
+    user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ error: error });
+  }
+});
+router.delete('/', withAuth, async(req, res) => {
+  try {
+    var user = await User.findOne({ _id: req.user._id })
+    await user.delete();
+    res.json({message: 'OK'}).status(201);
+  } catch (error) {
+    res.status(500).json({error: 'Problem to delete a note'});
   }
 });
 
