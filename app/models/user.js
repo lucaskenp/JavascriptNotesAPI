ecf3fc392567a69c4e6e4806780551697e57cf2f
Lucas Lima <lucasmonteirolima17@gmail.com>
2021-09-04 11:14:11 -0300

    Correção

--- app/models/user.js
+++ app/models/user.js
@@ -12,26 +12,24 @@ var userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function (next) {
     if (this.isNew || this.isModified('password')) {
-        const document = this;
-        bcrypt.hash(this.password, 10,
-            (err, hashedPassword) => {
-                if (err)
-                    next(err);
-                else {
-                    this.password = hashedPassword;
-                    next();
-                }
-
+        bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+            if (err)
+                next(err);
+            else {
+                this.password = hashedPassword;
+                next();
             }
+
+        }
         )
     }
 })
 
-userSchema.methods.isCorrectPassword = function (password, callback){
-    bcrypt.compare(password, this.password, function(err, same){
-        if(err){
+userSchema.methods.isCorrectPassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, same) {
+        if (err) {
             callback(err);
-        }else{ 
+        } else {
             callback(err, same);
         }
     })
